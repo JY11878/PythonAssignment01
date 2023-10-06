9359ec3a3e128c94f71e73e86ec64854e0acddf3
Xinhao Yao <113320951+JY11878@users.noreply.github.com>
2023-10-06 13:18:00 -0400

    Update LeapYear.py

--- LeapYear.py
+++ LeapYear.py
@@ -1,15 +1,14 @@
 #Define the main function where the logic of the program resides.
 def main():
-    #Prompt the user to enter a year to test.
+    # Prompt the user to enter a year to test.
     nyear = int(input("Which year you want to test?"))
-    #Calculate the remainder when the year is divided by 4.
-    x = nyear % 4
-    #Check if the year is divisible by 4.
-    if x == 0:
-        #If yes, print that it's a leap year.
+
+    # Check if the year is divisible by 4 and either not divisible by 100 or divisible by 400.
+    if (nyear % 4 == 0) and (nyear % 100 != 0 or nyear % 400 == 0):
         print("This year is a leap year!")
-    else:
         #Otherwise, print that it's not a leap year.
+    else:
         print("This year is NOT a leap year!")
-#Call the main function to execute the program.
+
+# Call the main function to execute the program.
 main()
